4929cf7a6b6372f26c41bef6304fe65f87e76979
leboMatlala <lebogangm@ejoobi.co.za>
2019-02-22 10:02:48 +0200

    2nd commit

--- routes.js
+++ routes.js
@@ -10,7 +10,7 @@ const requestHandler = (req, res) => {
 
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button><h1>My form</h1></form></body>');
         res.write('</html>');
         return res.end();
     }
