6f1aec9b2abe38739d165d7f5e5f092b08e44cb8
sibongilek <sibongilek@ejoobi.co.za>
2019-03-06 15:43:40 +0200

    sibongile

--- routes.js
+++ routes.js
@@ -10,6 +10,7 @@ const requestHandler = (req, res) => {
 
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
+         res.write('<head><title>Please Enter Message</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button><h1>My form</h1></form></body>');
         res.write('</html>');
         return res.end();
